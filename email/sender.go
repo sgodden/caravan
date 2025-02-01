d6f7bc1e7f7da0d72edbdb1f6e942c8df20d569f
Simon Godden <simon@blackcatsolutions.co.uk>
2025-02-01 15:18:02 +0000

    Add initial code to actually send the email

--- email/sender.go
+++ email/sender.go
@@ -1,13 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"net/smtp"
+)
 
 const smtpServer = "smtp.gmail.com"
-const smtpTlsPort = 587
+const smtpTlsPort = "587"
+const from = "glamorgan9quaywest@gmail.com"
 
 func sendEmail(emailBody string, contact *Contact, smtpSettings *SmtpSettings) {
-	fmt.Println(contact.Email)
-	fmt.Println(emailBody)
-
-	fmt.Println("Settings: ", smtpSettings)
+	auth := smtp.PlainAuth("", smtpSettings.userId, smtpSettings.password, smtpServer)
+	err := smtp.SendMail(smtpServer+":"+smtpTlsPort, auth, from, []string{contact.Email}, []byte(emailBody))
+	check(err)
 }
