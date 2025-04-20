597e8dac3f202eaf434bb98e59b95e6e50a617ec
Simon Godden <simon@blackcatsolutions.co.uk>
2025-04-20 19:08:57 +0100

    Refactoring

--- email/sender.go
+++ email/sender.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/smtp"
 )
 
@@ -8,8 +9,13 @@ const smtpServer = "smtp.gmail.com"
 const smtpTlsPort = "587"
 const from = "glamorgan9quaywest@gmail.com"
 
-func sendEmail(emailBody string, contact *Contact, smtpSettings *SmtpSettings) {
-	auth := smtp.PlainAuth("", smtpSettings.userId, smtpSettings.password, smtpServer)
-	err := smtp.SendMail(smtpServer+":"+smtpTlsPort, auth, from, []string{contact.Email}, []byte(emailBody))
-	check(err)
+func sendEmail(emailBody string, contact *Contact, smtpSettings *SmtpSettings, dryRun bool) {
+	fmt.Println("Sending to: ", contact.Email)
+	if !dryRun {
+		auth := smtp.PlainAuth("", smtpSettings.userId, smtpSettings.password, smtpServer)
+		err := smtp.SendMail(smtpServer+":"+smtpTlsPort, auth, from, []string{contact.Email}, []byte(emailBody))
+		check(err)
+	} else {
+		fmt.Println("Dry run, skipping email send")
+	}
 }
